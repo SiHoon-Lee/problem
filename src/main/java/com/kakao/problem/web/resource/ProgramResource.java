c618f4a227ae2706a24f7f944f6d530422861dca
sihoon <sihoon.lee87@gmail.com>
2019-04-15 09:11:39 +0900

    Change Build.gradle

--- src/main/java/com/kakao/problem/web/resource/ProgramResource.java
+++ src/main/java/com/kakao/problem/web/resource/ProgramResource.java
@@ -5,9 +5,7 @@ import com.kakao.problem.web.domain.ProgramBulkResponse;
 import com.kakao.problem.web.service.ProgramService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
@@ -20,7 +18,7 @@ public class ProgramResource {
     @Autowired
     private ProgramService programService;
 
-    @PostMapping(value = "/bulk")
+    @PostMapping("/bulk")
     public ResponseEntity<ProgramBulkResponse> saveBulk(ProgramBulkRequest programBulkRequest) throws IOException {
 
         Long recordCnt = programService.bulk(programBulkRequest);
